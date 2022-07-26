7b472c499eed1edaaa3580e35970a50ecc5e5b3f
Jarl G. Taxeraas Flaten <jtaxers@uwo.ca>
2022-07-26 14:05:38 +0200

    Add partial proof of abses_pushout_homotopic

--- BaerSumLaws.v
+++ BaerSumLaws.v
@@ -1,7 +1,8 @@
 From HoTT Require Import Basics Types Pointed.
 From HoTT Require Import Homotopy.ExactSequence WildCat.
 From HoTT Require Import AbGroups.AbelianGroup AbSES.Core AbSES.Pullback AbSES.Pushout.
-From HoTT Require Import BaerSum.
+From HoTT Require Import BaerSum AbGroups.AbPushout.
+
 Require Import AbProjective.
 
 (* Jacob: The swap isomorphism of the direct product of two abelian groups. *)
@@ -36,8 +37,54 @@ Proof.
     apply equiv_path_grouphomomorphism. reflexivity.
 Defined.
 
-(* Given a short exact sequence [A -> E -> B] and maps [f : A -> A'], [g : B' -> B], we can change the order of
-   pushing out along f and pulling back along g. *)
+(** We can check equality of maps out of a pushout on the two inclusions. *)
+Lemma abses_pushout_equal_components `{Univalence} {A B C Y : AbGroup}
+  (f : A $-> B) (g : A $-> C) (phi psi : ab_pushout f g $-> Y)
+  : ((phi o ab_pushout_inl == psi o ab_pushout_inl) *
+       (phi o ab_pushout_inr == psi o ab_pushout_inr))
+      <-> (phi == psi).
+Admitted.
+
+Lemma abses_pushout_homotopic' `{Univalence} {A A' B : AbGroup} (f f' : A $-> A') (h : f == f')
+  : abses_pushout0 (B:=B) f $=> abses_pushout0 f'.
+Proof.
+  intro E.
+  apply abses_path_data_to_iso.
+  snrefine (_; (_,_)).
+  - snrapply functor_ab_pushout.
+    1-3: apply grp_homo_id.
+    2: reflexivity.
+    exact h.
+  - intro x; simpl.
+    apply qglue.
+    apply tr.
+    exists mon_unit.
+    apply path_prod'; cbn.
+    + refine (grp_homo_unit _ @ _).
+      apply grp_moveL_gM.
+      exact (left_identity _ @ ap _ (right_identity _)^).
+    + rewrite 2 grp_unit_r.
+      exact (grp_homo_inv _ _ @ ap _ (grp_homo_unit _)).
+  - apply abses_pushout_equal_components; split.
+    + intro x.
+      refine (ab_pushout_rec_beta_left f (inclusion _) _ _ _ _ @ _).
+      1: symmetry; rapply cx_isexact.
+      simpl; symmetry.
+      refine (grp_unit_l _ @ ap (projection E) (grp_unit_l _) @ _).
+      apply grp_homo_unit.
+    + intro x.
+      refine (ab_pushout_rec_beta_right f (inclusion _) _ _ _ _ @ _).
+      1: symmetry; rapply cx_isexact.
+      simpl; symmetry.
+      exact (grp_unit_l _ @ ap (projection E) (grp_unit_l _)).
+Defined.
+
+Definition abses_pushout_homotopic `{Univalence} {A A' B : AbGroup}
+  (f f' : A $-> A') (h : f == f')
+  : abses_pushout0 (B:=B) f == abses_pushout0 f'
+  := equiv_path_data_homotopy _ _ (abses_pushout_homotopic' _ _ h).
+
+(* Given a short exact sequence [A -> E -> B] and maps [f : A -> A'], [g : B' -> B], we can change the order of pushing out along f and pulling back along g. *)
 
 (* Jacob: This is the first version of the proof - will look into reasoning backwards to avoid posing. *)
 
